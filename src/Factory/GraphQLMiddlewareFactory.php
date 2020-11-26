a8119983efa6a7e41748c512a2c81c731dabbc00
iampersistent <shank@zestic.com>
2020-11-25 22:34:04 -0800

    decode token before setting context

--- src/Factory/GraphQLMiddlewareFactory.php
+++ src/Factory/GraphQLMiddlewareFactory.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace IamPersistent\GraphQL\Middleware\Factory;
 
+use App\Jwt\JwtConfiguration;
 use GraphQL\Server\ServerConfig;
 use Psr\Container\ContainerInterface;
 use IamPersistent\GraphQL\Middleware\GraphQLMiddleware;
@@ -11,9 +12,10 @@ final class GraphQLMiddlewareFactory
 {
     public function __invoke(ContainerInterface $container): GraphQLMiddleware
     {
-        $serverConfig = $container->get(ServerConfig::class);
         $config = $container->get('config');
+        $jwtConfig = $container->get(JwtConfiguration::class);
+        $serverConfig = $container->get(ServerConfig::class);
 
-        return new GraphQLMiddleware($serverConfig, $config['graphQL']['middleware']);
+        return new GraphQLMiddleware($serverConfig, $jwtConfig, $config['graphQL']['middleware']);
     }
 }
